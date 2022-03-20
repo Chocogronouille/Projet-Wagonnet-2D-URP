8446008d80d5a037a88c08555e032717df8f1b99
DJ_Yazid <y.djaoui@rubika-edu.com>
2022-03-20 17:55:49 +0100

    Amélioration Des Rails 2

--- Projet Wagonnet/Assets/Scripts/Mecaniques LD/IndexScript.cs
+++ Projet Wagonnet/Assets/Scripts/Mecaniques LD/IndexScript.cs
@@ -6,6 +6,8 @@ public class IndexScript : MonoBehaviour
 {
     public int index = 0;
     public Transform parent;
+    public float posZ;
+    private float delayTime = 0.05f ;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,26 +17,22 @@ public class IndexScript : MonoBehaviour
         parent.gameObject.AddComponent<ListOfChild>();
         parent.GetComponent<ListOfChild>().Newindex = index;
         parent.GetComponent<ListOfChild>().enabled = true;
+        posZ = gameObject.transform.position.z ;
+    //    Destroy(gameObject.GetComponent<IndexScript>(),1f);
     //    gameObject.transform.SetSiblingIndex(index + 1); 
-    //   Destroy(gameObject,1);
+        Destroy(gameObject,0.1f);
+        StartCoroutine(LaCouroutine(delayTime));
     }
 
     // Update is called once per frame
     void Update()
     {
-        index = gameObject.transform.GetSiblingIndex();  
-   /*     if (index == 9)
-        {
-            gameObject.GetComponent<IndexScript>().enabled = false;
-        } */
-    /*     if(index == 0)
-        {
-        //    Debug.Log("0");
-        }
-        else
-        {
-           gameObject.transform.SetSiblingIndex(index + 1);
-        } */
         
     }
+
+    IEnumerator LaCouroutine(float delayTime)
+{
+   yield return new WaitForSeconds(delayTime);
+   Instaciate.instance.InstanciateTest();
+}
 }
