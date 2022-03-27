49d6fcd74848ea16bcacc9d0a769ffccc47977ae
DJ_Yazid <y.djaoui@rubika-edu.com>
2022-03-27 21:35:11 +0200

    AméliorationRails

--- Projet Wagonnet/Assets/RespawnObject.cs
+++ Projet Wagonnet/Assets/RespawnObject.cs
@@ -28,6 +28,7 @@ public class RespawnObject : MonoBehaviour
         parent = gameObject;
         TrackGenerate = GameObject.Find("TrackGenerator");
         Player = GameObject.FindWithTag("Player");;
+        new Waypoint { position = new Vector3(0, 0, -5), rotation = new Vector3(1, 0, 0) };
         
     //    Instantiate(unobject);
     //    unobject.AddComponent<makeparent>();
@@ -49,22 +50,29 @@ public class RespawnObject : MonoBehaviour
 
     void OnTriggerExit2D()
     {
+ /*       Debug.Log(RotationList[4].z);
+        foreach (Quaternion rot in RotationList) 
+        {
+         //   Debug.Log(rot.z);
+     //    Line.transform.rotation.z = rot.z;
+        } */
         foreach (Transform child in gameObject.transform) 
         {
          GameObject.Destroy(child.gameObject, 0.2f);
         }
         foreach (Vector3 pos in VectorList) 
         {
-         Instantiate(Line,new Vector3(pos.x, pos.y, pos.z), Quaternion.identity, parent.transform);
+        //    Debug.Log("rota.z");
+         Instantiate(Line,new Vector3(pos.x, pos.y, pos.z), new Quaternion(0,0,0,0), parent.transform);
          Line.GetComponent<makeparent>().parent = parent;
          StartCoroutine(Timer(delayTime));
     //     Line.transform.parent = parent.transform;
        //    Line.transform.SetParent(parent.transform);
-         foreach (Quaternion rot in RotationList) 
+        }
+     /*           foreach (Quaternion rot in RotationList) 
         {
-         Line.transform.rotation = rot;
-        }
-        }
+            gameObject.transform.GetChild(0).GetComponent<IndexRotation>().RotationList.Add(rot);
+        } */
     }
 
     IEnumerator Timer(float delayTime)
