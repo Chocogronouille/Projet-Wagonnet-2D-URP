a4d2179bb2611021356a9a36685b196cecc7bfa5
Chocogronouille <e.gonen@rubika-edu.com>
2022-03-29 02:23:30 +0200

    Tri-+-Anims

---
+++ Projet Wagonnet/Assets/Scripts/GameManager/OnGameLoad.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGameLoad : MonoBehaviour
+{
+    public Animator debutDeJeu;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        GameObject.Find("DebutDeJeu").SetActive(true);
+        debutDeJeu.SetTrigger("DebutDeJeu");
+    }
+}
