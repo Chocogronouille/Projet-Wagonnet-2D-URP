29c30de1de9116e3cc0c0e36c62dc2f039956647
Philippon Maxime <m.philippon@rubika-edu.com>
2022-03-31 10:38:56 +0200

    Modif Ground Check pour retirer un bug

--- Projet Wagonnet/Assets/Scripts/Player/GroundCheck.cs
+++ Projet Wagonnet/Assets/Scripts/Player/GroundCheck.cs
@@ -9,11 +9,14 @@ namespace Player
 
         private void OnTriggerStay2D(Collider2D other)      //Tant que le GroundCheck collide avec quelque-chose
         {
-            player.isAirborn = false;                       //Le joueur n'est plus en l'air
-            player.coyoteFloat = false;                     //Le joueur n'est pas en Coyote Time
-            player.canSpinJump = false;                     //Le joueur ne peut pas Spin Jump
-            player.isFalling = false;                       //Le joueur n'est pas en train de tomber
-            player.GetComponent<Rigidbody2D>().drag = 10;   //La friction du joueur passe à 10 pour l'arrêter lorsqu'il ne maintient plus son joystick dans une direction
+            if (player.GetComponent<Rigidbody2D>().velocity.y<=0)
+            {
+                player.isAirborn = false;                       //Le joueur n'est plus en l'air
+                player.coyoteFloat = false;                     //Le joueur n'est pas en Coyote Time
+                player.canSpinJump = false;                     //Le joueur ne peut pas Spin Jump
+                player.isFalling = false;                       //Le joueur n'est pas en train de tomber
+                player.GetComponent<Rigidbody2D>().drag = 10;   //La friction du joueur passe à 10 pour l'arrêter lorsqu'il ne maintient plus son joystick dans une direction
+            }
         }
     }
 }
