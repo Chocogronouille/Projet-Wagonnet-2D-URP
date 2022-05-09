054961139cd09d5d63726f26a2892fbe9ed3311e
Philippon Maxime <m.philippon@rubika-edu.com>
2022-05-09 17:52:20 +0200

    Ajout Parallax verticale

--- Projet Wagonnet/Assets/Scripts/Parallaxe/ParallaxScrolling.cs
+++ Projet Wagonnet/Assets/Scripts/Parallaxe/ParallaxScrolling.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ParallaxScrolling : MonoBehaviour
 {
-    private float _length, _startPosition, _distance, _ecartParallax;
+    private float _length, _startPositionX, _startPositionY, _distanceX, _distanceY, _ecartParallax;
     private Vector3 _newPosition;
     
     public float parallaxFactor;
@@ -12,7 +12,8 @@ public class ParallaxScrolling : MonoBehaviour
 
     void Start()
     {
-        _startPosition = transform.position.x;
+        _startPositionX = transform.position.x;
+        _startPositionY = transform.position.y;
         _length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
@@ -22,17 +23,18 @@ public class ParallaxScrolling : MonoBehaviour
         var positionCamPlayer = camPlayer.transform.position;
         
         _ecartParallax = positionCamPlayer.x * (1 - parallaxFactor);
-        _distance = positionCamPlayer.x * parallaxFactor;
-        _newPosition = new Vector3(_startPosition + _distance, transform.position.y, transform.position.z);
+        _distanceX = positionCamPlayer.x * parallaxFactor;
+        _distanceY = positionCamPlayer.y * parallaxFactor;
+        _newPosition = new Vector3(_startPositionX + _distanceX, _startPositionY + _distanceY, transform.position.z);
         transform.position = _newPosition;
-
-        if (_ecartParallax>_startPosition + (_length/2))
+ 
+        if (_ecartParallax>_startPositionX + (_length/2))
         {
-            _startPosition += _length;
+            _startPositionX += _length;
         }
-        else if(_ecartParallax < _startPosition - (_length/2))
+        else if(_ecartParallax < _startPositionX - (_length/2))
         {
-            _startPosition -= _length;
+            _startPositionX -= _length;
         }
     }
 }
