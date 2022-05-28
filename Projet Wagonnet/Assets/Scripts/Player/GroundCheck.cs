41cf9d6359c62eaa51e4dbb26b39a82bc44e7985
DJ_Yazid <y.djaoui@rubika-edu.com>
2022-05-29 01:02:25 +0200

    FX de marche

--- Projet Wagonnet/Assets/Scripts/Player/GroundCheck.cs
+++ Projet Wagonnet/Assets/Scripts/Player/GroundCheck.cs
@@ -8,8 +8,22 @@ namespace Player
 {
     public class GroundCheck : MonoBehaviour
     {
+        private GameObject Player;
         [SerializeField] private PlayerInput player;
         public ParticleSystem Effects;
+  /*      private ParticleSystem WalkEffectsRight;
+        private ParticleSystem WalkEffectsLeft; */
+        private GameObject WalkEffectsRight;
+        private GameObject WalkEffectsLeft;
+
+        void Start()
+        {
+            Player = GameObject.Find("Player");
+     /*       WalkEffectsRight = GameObject.Find("VFX_Dust_Right").GetComponent<ParticleSystem>();
+            WalkEffectsLeft = GameObject.Find("VFX_Dust_Left").GetComponent<ParticleSystem>(); */
+            WalkEffectsRight = GameObject.Find("VFX_Dust_Right");
+            WalkEffectsLeft = GameObject.Find("VFX_Dust_Left");
+        }
         private void OnTriggerEnter2D(Collider2D other)
         {
             player.ResetMaxSpeed();
@@ -25,6 +39,18 @@ namespace Player
             player.isFalling = false;
             player.GetComponent<Rigidbody2D>().drag = player.groundDrag;
             Gamepad.current.SetMotorSpeeds(0f, 0f);
+            if(player.GetComponent<SpriteRenderer>().flipX)
+            {
+           //     WalkEffectsLeft.Play();
+                  WalkEffectsRight.SetActive(true);
+                  WalkEffectsLeft.SetActive(false);
+            }
+            else
+            {
+          //      WalkEffectsRight.Play();
+                  WalkEffectsRight.SetActive(false);
+                  WalkEffectsLeft.SetActive(true);
+            }
             
             player.ResetSpinJump();
         }
