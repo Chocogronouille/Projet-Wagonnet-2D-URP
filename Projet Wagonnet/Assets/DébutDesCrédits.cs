fa6531951bbb416fc371f6e8aa358dc2a6c26a1d
Philippon Maxime <m.philippon@rubika-edu.com>
2022-06-10 17:02:02 +0200

    1er changement texte + couleur crédits

--- Projet Wagonnet/Assets/DébutDesCrédits.cs
+++ Projet Wagonnet/Assets/DébutDesCrédits.cs
@@ -6,22 +6,9 @@ public class DébutDesCrédits : MonoBehaviour
 {
 
     public GameObject credits;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         credits.SetActive(true);
     }
-
-
 }
